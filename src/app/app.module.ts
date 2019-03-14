5ac0a5d25cfdc8faa7cfaedbf847e41a806ace6e
KPrado <32308796+KPrado@users.noreply.github.com>
2019-03-13 22:43:50 -0300

    add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+//Import component agGridModule 
+import { AgGridModule } from 'ag-grid-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AgGridModule.withComponents(null)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
